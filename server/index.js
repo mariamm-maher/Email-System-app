bb5e81b3717c9939955253eeea011bce169a3241
mariam maher <mahermariam367@gmail.com>
2024-11-22 17:43:44 +0200

    created new function

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-
+app.get("/home ", (req, res) => {
+  res.send("Server is running");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
