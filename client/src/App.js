eb2ff855a2b624ae3c0ff9fd3519f65e2b2e78d1
mariam maher <mahermariam367@gmail.com>
2024-11-25 20:24:17 +0200

    edited the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUpPage from "./pages/signUp";
+import LoginPage from "./pages/Login";
 import WelcomePage from "./pages/welcome";
-import SignUp from "./pages/signUp";
-import SignIn from "./pages/Login";
-import AdminDashboard from "./pages/Admin";
+import HomePage from "./pages/Home";
+import Profile from "./pages/UserProfile";
+import Admin from "./pages/Admin";
 export default function App() {
   return (
-    <div>
-      <WelcomePage />
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,38 +1,20 @@
 import React from "react";
 
-function profile()
-{
-    return(
-
-        <>
-        {/* div as a body */}
-    <div className="fixed inset-0 ">  
-
+function Profile() {
+  return (
+    <>
+      {/* div as a body */}
+      <div className="fixed inset-0 ">
         <div className="absolute bg-gradient-to-r from-neonMintGreen to-darkNavyBlue opacity-30 blur-3xl w-[400px] h-[400px] rounded-full top-10 left-10"></div>
         <div className="absolute bg-gradient-to-r from-darkNavyBlue to-neonMintGreen opacity-40 blur-3xl w-[500px] h-[500px] rounded-full top-1/2 right-10"></div>
         <div className="absolute bg-gradient-to-r from-neonMintGreen to-darkNavyBlue opacity-20 blur-3xl w-[450px] h-[450px] rounded-full bottom-10 left-1/3"></div>
 
-
-
         <h1 className=" ml-10px lg:text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-neonMintGreen to-PureWhite font-sans tracking-tight z-10">
-        Mailer Account 
+          Mailer Account
         </h1>
-
-
-    </div>
-
-
-        
-        </>
-
-
-
-
-    );
-
-
-
-
+      </div>
+    </>
+  );
 }
 
-export default profile;
+export default Profile;
